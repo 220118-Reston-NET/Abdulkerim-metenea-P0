c6ccfef65e1f752b7b9bd8dbf7a39dc6420ec70d
Your Name <you@example.com>
2022-02-02 15:00:53 -0500

    ER-Diagram_add

---
+++ storeUI/bin/LineItems.cs
@@ -0,0 +1,19 @@
+
+namespace storeUI
+{
+    public class LineItems : Products
+    {
+        public string Products;
+        private int Quantity;
+        public string Products
+        {
+            get { return Products; }
+
+        }
+        public int Quantity
+        {
+            get { return Count.Products; }
+        }
+    }
+}
+
